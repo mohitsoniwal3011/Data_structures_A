25a852502d728d664f10b680cd66bc0b562dd68a
mohitsoniwal3011 <mohitsoniwal3011@gmail.com>
2020-09-14 21:40:26 +0530

    selection sort by exchanging data of the nodes

---
+++ single_linked_list/selection_sort_by_exchanging_data.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+	int data;
+	struct node * next;
+};
+struct node * create_list()
+{
+	int i,n;
+	printf("enter the number of nodes you want to create \n");
+	scanf("%d",&n);
+	if(n==0){
+		struct node *head=NULL;
+		return head;
+	}	
+	struct node *head,*newnode,*temp;
+	/*we will use newnode for creating any new node in the list.
+	head to store the address of the first node so that we can return it to the main function
+	temp to traverse through the list so that any other links in the list do not get Damaged. */
+	//creating  the head node
+	newnode=(struct node *)malloc(sizeof(struct node));
+	i=1;
+	head=NULL;
+	printf("enter the data for node %d :\n",i);
+	scanf("%d",&newnode->data);
+	newnode->next=NULL;
+	head=newnode;
+	temp=head; 
+	//creating other nodes 
+	for(i=2;i<=n;i++){
+		newnode=(struct node *)malloc(sizeof(struct node));
+		printf("enter the data for node %d:\n",i);
+		scanf("%d",&newnode->data);
+		newnode->next=NULL;
+		temp->next=newnode;
+		temp=newnode;
+	}
+	return head;
+}
+struct node *selection_sort(struct node *start)
+{
+	if(start ==NULL){
+		printf("list is empty");
+		return start ;
+	}
+	struct node *p,*temp;
+	int min ;
+	for(p=start;p != NULL;p=p->next){
+		min=p->data;
+		for(temp=p->next;temp != NULL;temp=p->next){
+			if((temp->data)<min){
+				swap(&p->data,&temp->data);
+			}
+		}
+	}
+}
+void swap(int *a,int *b)
+{
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}	
+void display(struct node *start)
+{
+	struct node *p;//for traversing through the list
+	p=start;
+	int i=1;
+	if(start ==NULL){
+		printf("list is empty");
+		return ;
+	}	
+	while(p != NULL){
+		printf("\ndata of the node %d :\n",i++);
+		printf("%d",p->data);
+		p=p->next;
+	}	
+	return ;
+}	
+
+int main ()
+{
+	struct node *head=(struct node *)malloc(sizeof(struct node));
+	head=create_list();
+	selection_sort(head);
+	struct node *new_head=(struct node *)malloc(sizeof(struct node));
+	display(new_head);
+	return 0;
+}
+ 	
+
+		
+	
+	
+	
+	
+	
